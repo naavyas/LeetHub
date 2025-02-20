6625e0d716000ac0f10ddecc9b5c57076ad27466
naavyas <82998703+naavyas@users.noreply.github.com>
2025-02-19 23:02:02 -0500

    File updated at 2/19/2025, 11:02:02 PM

--- 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.py
+++ 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.py
@@ -8,25 +8,7 @@
 basic revision of bfs - breadth first search
 have a queue and keep adding the elements per level of the tree 
 we can have a pointer that helps keep track of the length of the tree
- def dfs(root,len):
-        if root == None:
-            return 
-        if root.left == None and root.right == None:
-            return len
-        else:
-            if root.left!=None:
-                l = self.dfs(root.left, len+1)
-            if root.right!=None:
-                r = self.dfs(root.right,len+1)
-        if l<=r:
-            return l
-        else:
-            return r
-"""
-class Solution:
-    
-    def minDepth(self, root: Optional[TreeNode]) -> int:
-        queue = []
+queue = []
         queue.append(root)
         ctr = 0
         if root == None:
@@ -44,5 +26,43 @@ class Solution:
                 if var.right!=None:
                     queue.append(var.right)
         return ctr
+ def dfs(root,len):
+        if root == None:
+            return 
+        if root.left == None and root.right == None:
+            return len
+        else:
+            if root.left!=None:
+                l = self.dfs(root.left, len+1)
+            if root.right!=None:
+                r = self.dfs(root.right,len+1)
+        if l<=r:
+            return l
+        else:
+            return r
+"""
+class Solution:
+    def dfs(self, root,len):
+        if root == None:
+            return 0
+        if root.left == None and root.right == None:
+            return len
+        else:
+            if root.left!=None:
+                l = self.dfs(root.left, len+1)
+            elif root.left == None:
+                l = float('inf')
+            if root.right!=None:
+                r = self.dfs(root.right,len+1)
+            elif root.right == None:
+                r = float('inf')
+        if l<=r:
+            return l
+        else:
+            return r
+    
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        return self.dfs(root,1)
+        
         
         
