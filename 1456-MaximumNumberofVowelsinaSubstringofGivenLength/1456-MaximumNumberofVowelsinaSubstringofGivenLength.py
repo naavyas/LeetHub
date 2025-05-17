ebe5f42ec863ff431b4de395d06c74f15071a449
naavyas <82998703+naavyas@users.noreply.github.com>
2025-05-16 23:10:08 -0400

    File updated at 5/16/2025, 11:10:08 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.py
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.py
@@ -1,35 +1,48 @@
-# Last updated: 5/16/2025, 10:43:59 PM
+# Last updated: 5/16/2025, 11:10:08 PM
+"""
+return the max number vowels 
+fixed length of substring given as k 
+vowels : a e i o u
+input: s = 'abciiidef' 
+k = 3
+first we go through the first 3 letters 
+we keep a count of the number of vowels
+we also have a max_vowel variable to help us keep track of the max
+we keep going from left to right and as we move we change the vowel count and only update the max variable if the new count is greater 
+we then finally return the max_count variable 
+"""
+from collections import defaultdict
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = ['a', 'e', 'i', 'o','u']
-        right = 0
-        left = 0 
-        max_count = 0
-        vowel_count = 0
-        for r in range(k):
-            if s[r] in vowels:
-                vowel_count += 1
-        max_count = max(max_count, vowel_count)
-        right = k-1
+        vowels = list()
+        vowels = ['a', 'e', 'i', 'o', 'u']
         left = 0
+        right = 0
+        vowel_count = 0 
+        max_vowel = 0
+        for r in range(k): #loop to go through the fixed part of the string first
+            if s[right] in vowels:
+                vowel_count += 1
+            right+=1
+        max_vowel = max(max_vowel,vowel_count)
+        right = k - 1
         while right<len(s):
             right += 1
-            while right<len(s) and left<right and right - left + 1 > k :
-                if s[left] in vowels:
-                    vowel_count -= 1
-                left+=1
+            while left<right and right<len(s) and right-left == k:
                 if s[right] in vowels:
                     vowel_count += 1
-            max_count = max(max_count, vowel_count)
-        return max_count
+                if s[left] in vowels:
+                    vowel_count -= 1
+                left+= 1
+            max_vowel = max(max_vowel,vowel_count)
 
-"""
-l e e t c o d e.      k = 3
-vowelcount = 1 
-right = 5
-left = 1
-max = 2
-"""
+        return max_vowel
+
+
+
+            
+
+
 
 
         
