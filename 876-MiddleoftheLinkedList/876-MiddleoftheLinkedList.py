aeffc6f45be506c73b67f694816dc944ab4eaca4
naavyas <82998703+naavyas@users.noreply.github.com>
2025-05-23 19:39:10 -0400

    File updated at 5/23/2025, 7:39:09 PM

--- 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.py
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.py
@@ -1,20 +1,22 @@
-# Last updated: 3/25/2025, 12:00:32 AM
+# Last updated: 5/23/2025, 7:39:09 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
+the way we can reach of the  middle of the linked list is through 
+iterating through the list using the pointers 
+so we move fast faster than the next and it reaches the end first
+"""
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #dry run: [1,2,3,4,5] // even no of nodes
-        #[1]
-        #slow = 3
-        #fast = 5
-        if head.next == None:
-            return head
+
         slow = head 
         fast = head 
-        while fast != None and fast.next != None :
+        if slow.next == None:
+            return slow
+        while fast and fast.next!=None:
             slow = slow.next 
             fast = fast.next.next 
         return slow 
